4768ca1ebb0dbdc408a77980dc651b06d1b66826
Acivico <amun2594@gmail.com>
2021-10-18 12:56:08 +0200

    download file from s3 url

--- downloadFile-s3.py
+++ downloadFile-s3.py
@@ -1,7 +1,5 @@
 import requests
 
-
-# url = '/giab/data/NA12878/NIST_NA12878_HG001_HiSeq_300x/140407_D00360_0017_BH947YADXX/Project_RM8398/Sample_U5c/U5c_CCGTCC_L001_R2_001.fastq.gz'
 url = 's3://giab/data/NA12878/NIST_NA12878_HG001_HiSeq_300x/140407_D00360_0017_BH947YADXX/Project_RM8398/Sample_U5c/U5c_CCGTCC_L001_R2_001.fastq.gz'
 
 def downloadFile_s3(url, local_dir):
