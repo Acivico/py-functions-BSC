4c7a5f152bebca0ab65a0d4dd41d0777145ae5a1
Acivico <amun2594@gmail.com>
2021-10-20 12:55:44 +0200

    Update to throw exceptions

--- downloadFolder-s3.py
+++ downloadFolder-s3.py
@@ -2,7 +2,10 @@ import boto3
 from botocore.handlers import disable_signing
 from urllib.parse import urlparse
 import os
+import logging
 import argparse
+
+logger = logging.getLogger('downloadFolder-s3')
 
 def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
     s3_resource = boto3.resource('s3')
@@ -12,15 +15,18 @@ def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
         if not os.path.exists(os.path.dirname(obj.key)):
             os.makedirs(os.path.dirname(obj.key))
         bucket.download_file(obj.key, obj.key) # save to same path
+try:
+    parser = argparse.ArgumentParser(description='This is the function to download folder from aws s3')
+    parser.add_argument('-u',action='store',dest='url',default=None,help='<Required> URL link',required=True)
+    results = parser.parse_args()
+    url = results.url
 
-parser = argparse.ArgumentParser(description='This is the function to download folder from aws s3')
-parser.add_argument('-u','--ur',action='store',dest='url',default=None,help='<Required> URL link',required=True)
-results = parser.parse_args()
-url = results.url
+    url = urlparse(url)
+    bucket = url.netloc
+    directory = url.path
+    directory = directory[1:]
 
-url = urlparse(url)
-bucket = url.netloc
-directory = url.path
-directory = directory[1:]
+    downloadDirectoryFroms3(bucket,directory)
+except botocore.exceptions.ParameterError as error:
+    logger.error("Error downloading file" + error)
 
-downloadDirectoryFroms3(bucket,directory)
