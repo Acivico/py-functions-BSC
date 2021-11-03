b561872e0c8b7275f22a0c84992657c54955e7e1
Acivico <amun2594@gmail.com>
2021-11-03 09:52:48 +0100

    added option to download private buckets

--- downloadFile-s3.py
+++ downloadFile-s3.py
@@ -1,5 +1,5 @@
 import boto3
-import botocore
+import botocore.exceptions
 from botocore import UNSIGNED
 from botocore.client import Config
 from urllib.parse import urlparse
@@ -12,22 +12,26 @@ import logging
         file_name => path of file in bucket
         local_path => directory in which to save the file
 """
-def downloadFile_s3(bucket, file_name, local_path):
-    s3.download_file(bucket, file_name, local_path)
+def downloadFile_s3(bucket, file_name, local_path, access_key=None, secret_key=None):
+    if access_key == None and secret_key == None:
+        s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+        s3.download_file(bucket, file_name, local_path)
+    else:
+        s3 = boto3.client('s3', access_key, secret_key)
+        s3.download_file(bucket, file_name, local_path)
 
 logger = logging.getLogger('downloadFile-s3')
 
 parser = argparse.ArgumentParser(description='This is the function to download file from aws s3')
 parser.add_argument('-u','--ur',action='store',dest='url',default=None,help='<Required> URL link',required=True)
 parser.add_argument('-l',action='store',dest='local_path',default=None,help='<Required> Local directory for save file',required=True)
+parser.add_argument('-a','--ak',action='store',dest='access_key',default=None,help='Credential for s3 bucket (access key)')
+parser.add_argument('-s','--sk',action='store',dest='secret_key',default=None,help='Credential for s3 bucket (secret key)')
 results = parser.parse_args()
 url = results.url
 local_path = results.local_path
-
-try:
-    s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-except botocore.exceptions.ClientError as error:
-    logger.error("Anon conection failed" + error)
+access_key = results.access_key
+secret_key = results.secret_key
     
 try:
     url = urlparse(url)
@@ -44,6 +48,6 @@ try:
     elif(local_path[:1] != '/'):
         local_path = local_path + '/' + file_name
     
-    downloadFile_s3(bucket, file_bucket, local_path)
+    downloadFile_s3(bucket, file_bucket, local_path, access_key, secret_key)
 except botocore.exceptions.ParamValidationError as error:
     logger.error("Error downloading file" + error)
--- downloadFolder-s3.py
+++ downloadFolder-s3.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore.exceptions
 from botocore.handlers import disable_signing
 from urllib.parse import urlparse
 import os
@@ -7,26 +8,49 @@ import argparse
 
 logger = logging.getLogger('downloadFolder-s3')
 
-def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
-    s3_resource = boto3.resource('s3')
-    s3_resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
-    bucket = s3_resource.Bucket(bucketName) 
-    for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
-        if not os.path.exists(os.path.dirname(obj.key)):
-            os.makedirs(os.path.dirname(obj.key))
-        bucket.download_file(obj.key, obj.key) # save to same path
+def downloadDirectoryFroms3(bucketName, remoteDirectoryName, local_dir, access_key=None, secret_key=None):
+    if access_key == None and secret_key == None:
+        s3_resource = boto3.resource('s3')
+        s3_resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
+        bucket = s3_resource.Bucket(bucketName) 
+        for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
+            if not os.path.exists(os.path.dirname(obj.key)):
+                os.makedirs(os.path.dirname(obj.key))
+            bucket.download_file(obj.key, obj.key) # save to same path
+    else:
+        try:
+            s3_resource = boto3.resource('s3', access_key, secret_key)
+            bucket = s3_resource.Bucket(bucketName) 
+            for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
+                if not os.path.exists(os.path.dirname(obj.key)):
+                    os.makedirs(os.path.dirname(obj.key))
+                bucket.download_file(obj.key, obj.key) # save to same path
+        except Exception as e:
+            logger.warn("ERROR when downloading the folder" + e)
+
 try:
     parser = argparse.ArgumentParser(description='This is the function to download folder from aws s3')
     parser.add_argument('-u',action='store',dest='url',default=None,help='<Required> URL link',required=True)
+    parser.add_argument('-l',action='store',dest='local_path',default=None,help='Local directory for save file')
+    parser.add_argument('-a','--ak',action='store',dest='access_key',default=None,help='Credential for s3 bucket (access key)')
+    parser.add_argument('-s','--sk',action='store',dest='secret_key',default=None,help='Credential for s3 bucket (secret key)')
     results = parser.parse_args()
     url = results.url
+    local_dir = results.local_path
+    access_key = results.access_key
+    secret_key = results.secret_key
 
     url = urlparse(url)
     bucket = url.netloc
     directory = url.path
     directory = directory[1:]
+    
+    if(access_key != "" and secret_key != ""):
+        downloadDirectoryFroms3(bucket,directory, local_dir, access_key, secret_key)
+    else:
+        downloadDirectoryFroms3(bucket,directory,local_dir)
 
-    downloadDirectoryFroms3(bucket,directory)
+
 except botocore.exceptions.ParameterError as error:
     logger.error("Error downloading file" + error)
 
